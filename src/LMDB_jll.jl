157728fdd8ff1504dd36ea5390c40979b986c570
jlbuild <juliabuildbot@gmail.com>
2026-05-22 12:36:33 +0000

    LMDB_jll build 0.9.35+0

--- src/LMDB_jll.jl
+++ src/LMDB_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("LMDB")
-JLLWrappers.@generate_main_file("LMDB", UUID("6206cf0b-f360-5984-af49-5437264c140e"))
+JLLWrappers.@generate_main_file("LMDB", Base.UUID("6206cf0b-f360-5984-af49-5437264c140e"))
 end  # module LMDB_jll
